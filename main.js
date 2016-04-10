4db7cd08de2b00a29e53101341e47ddad85ab9f6
DosseyRichards <Richardsdossey@gmail.com>
2016-04-10 15:07:34 -0400

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
- var myapp = angular.module('myApp', ["ui.router", "ngAnimate"])
-    myapp.config(function($stateProvider, $urlRouterProvider){
+ var Molliapp = angular.module('myApp', ["ui.router", "ngAnimate"])
+    Molliapp.config(function($stateProvider, $urlRouterProvider){
       
       // For any unmatched url, send to /route1
       $urlRouterProvider.otherwise("/route1")
@@ -9,16 +9,15 @@
       $stateProvider
         .state('route2', {
             url: "/route2",
-            templateUrl: "/route2",
+            templateUrl: "route2",
             controller: function($scope,$stateParams){
 
             }
         })
         .state('route1', {
             url: "/route1",
-            templateUrl: "/home",
+            templateUrl: "home",
             controller: function($scope,$stateParams){
-
           
         }
     })
